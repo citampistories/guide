ab8315af7f4d9fac9075db7db0edd7e6741c516e
glyphprism <glyphprism@gmail.com>
2026-06-22 00:09:19 +0700

    Create database/character.js

---
+++ database/character.js
@@ -0,0 +1,19 @@
+const characters = [
+  {
+    id: 1,
+    namaList: "Isma",
+    namaDetail: "Isma nur annisa",
+    foto: "image/character/isma.jpg",
+    pekerjaan: "Kasir Indomarket",
+    hobi: "Membaca",
+    barangSpesial: ["Parfum"],
+    barangFavorit: ["Parfum"],
+    kencanKedua: ["Parfum"],
+    hintHati: [
+      "Hint 1: Sapa dia saat pagi hari.",
+      "Hint 2: Berikan hadiah di jam sekolah.",
+      "Hint 3: Sering berbicara saat hari libur.",
+      "Hint 4: Bawa barang favorit saat event."
+    ]
+  }
+];
